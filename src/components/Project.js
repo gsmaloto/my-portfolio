25f60d3bf5b03b919657896034520d40b34cd73d
gsmaloto <genermaloto@gmail.com>
2022-08-21 14:14:49 +0800

    update

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,34 +1,47 @@
 import React from "react";
 import "./Project.css";
 import ProjectCard from "./ProjectCard";
-import Jump from "react-reveal/Jump";
 import movieWorld from '../assets/movieWorld.jpg'
+import yt from '../assets/yt.png'
+import axfitness from '../assets/axfitness.png'
 
 const Project = () => {
   return (
     <div className="project">
       <h1 className="project__title">My Projects</h1>
       <div className="project__box">
-        <Jump>
           <ProjectCard
             title="Random Advice Generator"
             image={require("../assets/random-advice.jpg")}
             description="ldjhaksjdhaksghdkajshdkjh"
             tech="HTML - CSS - Javascript"
-            code="https://www.google.com/"
+            code="https://github.com/gsmaloto/random-advice"
             demo="https://randomadvicee.netlify.app/"
           />
-        </Jump>
-        <Jump delay={200}>
           <ProjectCard
             title="Movie World"
             image={movieWorld}
             description="This is my first project using react. I used OMDB API for the movies."
             tech='HTML - CSS - ReactJS'
-            code="https://www.google.com/"
-            demo="https://www.youtube.com/"
+            code="https://github.com/gsmaloto/movie-world"
+            demo="https://gsmaloto.github.io/movie"
           />
-        </Jump>
+           <ProjectCard
+            title="Youtube Clone"
+            image={yt}
+            description="This project is user interface and dont have a."
+            tech='HTML - CSS - MaterialUI - ReactJS'
+            code="https://github.com/gsmaloto/youtube-clone"
+            demo="https://gsmaloto.github.io/youtube-clone/"
+          />
+          <ProjectCard
+            title="AX Fitness Appointment & Scheduling System"
+            image={axfitness}
+            description="This is my first project using react. I used OMDB API for the movies."
+            tech='HTML - CSS - ReactJS'
+            code="https://github.com/gsmaloto/axfitness"
+            demo=""
+          />
       </div>
     </div>
   );
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -15,6 +15,10 @@ const ProjectCard = ({ title, image, description, tech, code, demo }) => {
         <div className="projectCard__tech">
           <p>{tech}</p>
         </div>
+        <a href={code} className="projectCard__button">
+          <CodeIcon />
+          <span>Source Code</span>
+        </a>
         <a href={demo} className="projectCard__button">
           <VisibilityIcon />
           <span>Live Demo</span>
